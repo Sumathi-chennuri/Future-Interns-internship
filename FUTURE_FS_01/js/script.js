4f6f425de0150811bdeb6208c7c982e8e2c37822
Chennuri Sumathi <sumachennuri523@gmail.com>
2026-04-20 22:56:57 +0530

    Update script.js

--- FUTURE_FS_01/js/script.js
+++ FUTURE_FS_01/js/script.js
@@ -1,4 +1,1 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Thank you for your message! I will get back to you soon.");
-});
+console.log("Portfolio Loaded");
