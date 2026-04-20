bb20776c6e70295acddd360f912245a3d8eed81b
Chennuri Sumathi <sumachennuri523@gmail.com>
2026-04-20 23:29:40 +0530

    Update server.js

--- FUTURE_FS_02/server.js
+++ FUTURE_FS_02/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 let leads = [];
 
-app.get('/leads', (req, res) => {
+app.post('/add', (req, res) => {
+    leads.push(req.body);
+    res.send("Added");
+});
+
+app.get('/all', (req, res) => {
     res.json(leads);
 });
 
-app.post('/leads', (req, res) => {
-    leads.push(req.body);
-    res.send("Lead Added");
-});
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Server running"));
