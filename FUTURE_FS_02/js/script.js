03bdfce088e50d84bba8f4af37ad4f834ee2a8ed
Chennuri Sumathi <sumachennuri523@gmail.com>
2026-04-20 23:00:38 +0530

    Update script.js

--- FUTURE_FS_02/js/script.js
+++ FUTURE_FS_02/js/script.js
@@ -1,6 +1,26 @@
-let cartCount = 0;
+function addLead() {
+    const name = document.getElementById("name").value;
+    const status = document.getElementById("status").value;
 
-function addToCart() {
-  cartCount++;
-  document.getElementById("count").innerText = cartCount;
+    fetch('http://localhost:3000/leads', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name, status})
+    });
+
+    loadLeads();
+}
+
+function loadLeads() {
+    fetch('http://localhost:3000/leads')
+    .then(res => res.json())
+    .then(data => {
+        let list = document.getElementById("list");
+        list.innerHTML = "";
+        data.forEach(l => {
+            let li = document.createElement("li");
+            li.textContent = l.name + " - " + l.status;
+            list.appendChild(li);
+        });
+    });
 }
